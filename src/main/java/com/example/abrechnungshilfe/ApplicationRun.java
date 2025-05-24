fa31f2d41868572e5cda741d3fb1917d06fc9030
Niklas <jan-niklas-bremer@web.de>
2025-05-24 22:55:00 +0200

    ReName of classes

--- src/main/java/com/example/abrechnungshilfe/ApplicationRun.java
+++ src/main/java/com/example/abrechnungshilfe/ApplicationRun.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationRun extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationRun.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 580);
         stage.setTitle("Hello!");
         stage.setScene(scene);
