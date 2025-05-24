c178b1050d074b3f0f018ebb60df3060e2745b92
Niklas <jan-niklas-bremer@web.de>
2025-05-24 23:44:44 +0200

    First changes implemented nothing serios

--- src/main/java/com/example/abrechnungshilfe/MainViewController.java
+++ src/main/java/com/example/abrechnungshilfe/MainViewController.java
@@ -1,14 +1,35 @@
 package com.example.abrechnungshilfe;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 
-public class MainViewController {
+
+public class MainViewController
+{
     @FXML
-    private Label welcomeText;
+    private Label label;
+    @FXML
+    private Button CoinCalcButton;
+    @FXML
+    private TextField TF_2Euro, TF_1Euro, TF_50Cent, TF_20CEnt, TF_10Cent, TF_5Cent, TF_2Cent, TF_1Cent;
+    @FXML
+    private TextField TF_20Kr, TF_10Kr, TF_5Kr, TF_2Kr, TF_1Kr, TF_50Öre;
+
+
+    int EuroCoinTotal;
+    int DanishCoinTotal;
+
+    //Needs to calc all the coins together both Euro and Kr.
+    //then later on it needs to calculate the best way to get to exactly 200Euro and 1000DKR in the register
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void calcCoin()
+    {
+
+
+
     }
 }
